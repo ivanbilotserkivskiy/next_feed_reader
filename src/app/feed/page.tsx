b9af917788b6efde80249ca5f1d84009d9c8a6b4
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-10-26 14:38:01 +0300

    add redit data fetch

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import withAuth from "@/HOCs/withAuth";
+import { useEffect } from "react";
 
 const FeedPage = () => {
+  const getData = async () => {
+    fetch("https://www.reddit.com/.rss").then((res) => console.log(res));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <h1>Feed Page</h1>;
 };
 
